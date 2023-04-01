b7840c9dff5aa59ccee13be876f29eb0c0ed8633
somu3341 <somashekarvsv1996@gmail.com>
2023-04-01 17:12:10 +0530

    [somu]Add:Validating All Email Given by User

--- UnitTesting-UserRegistration/User.cs
+++ UnitTesting-UserRegistration/User.cs
@@ -8,6 +8,7 @@ namespace UnitTesting_UserRegistration
         public static string EMAIL = "^[A-Za-z]{3,}[.]{0,}[a-zA-Z]{3,}[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}[.]{1}[a-z]{2,}$";
         public static string MOBILE = "^[0-9]{2}\\s[6-9]{1}[0-9]{9}$";
         public static string PASSWORD = "^[A-Za-z]{4,}[!+@+#+$+%+^+&+*+(+)+_+++~+.+,]{1,}[0-9]{3,}$";
+        public static string ALLEMAIL = "^[a-zA-Z]+[.+_]{1}[a-zA-Z]+[@]{1}[a-z]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$$";
         public string UserFirst(string name)
         {
             if (Regex.IsMatch(name, USER_NAME))
@@ -38,5 +39,15 @@ namespace UnitTesting_UserRegistration
                 return "Valid";
             return "InValid";
         }
+        public string UserAllEmail(string[]arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                string word = arr[i];
+                if (Regex.IsMatch(word, ALLEMAIL))
+                    return "Valid";
+            }            
+            return "InValid";
+        }         
     }
 }
