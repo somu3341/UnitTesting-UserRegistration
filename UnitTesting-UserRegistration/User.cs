4f796fdcb3023d39e261695a3b0d7f0952af75e9
somu3341 <somashekarvsv1996@gmail.com>
2023-04-01 15:23:58 +0530

    [somu]Add:Validating First Name

---
+++ UnitTesting-UserRegistration/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UnitTesting_UserRegistration
+{
+    public class User
+    {
+        public static string USER_NAME = "^[A-Z]{1}[a-z]{2,}";
+        public string UserFirst(string name)
+        {
+            if (Regex.IsMatch(name, USER_NAME))
+                return "Valid";
+            return "InValid";
+        }
+    }
+}
