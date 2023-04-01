7a7f470928be8a0794ba8bfe372f58123e64340b
somu3341 <somashekarvsv1996@gmail.com>
2023-04-01 15:44:16 +0530

    [somu]Add:Validating User Email

--- UnitTesting-UserRegistration/User.cs
+++ UnitTesting-UserRegistration/User.cs
@@ -5,15 +5,22 @@ namespace UnitTesting_UserRegistration
     public class User
     {
         public static string USER_NAME = "^[A-Z]{1}[a-z]{2,}";
+        public static string EMAIL = "^[A-Za-z]{3,}[.]{0,}[a-zA-Z]{3,}[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}[.]{1}[a-z]{2,}$";
         public string UserFirst(string name)
         {
             if (Regex.IsMatch(name, USER_NAME))
                 return "Valid";
             return "InValid";
         }
-        public string UserSecond(string name)
+        public string UserLast(string name)
         {
             if (Regex.IsMatch(name, USER_NAME))
+                return "Valid";
+            return "InValid";
+        }
+        public string Email(string email)
+        {
+            if (Regex.IsMatch(email, EMAIL))
                 return "Valid";
             return "InValid";
         }
