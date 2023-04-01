0dfba891fb4d31f2488cca3a30c80580d717a97b
somu3341 <somashekarvsv1996@gmail.com>
2023-04-01 16:07:58 +0530

    [somu]Add:Validating User Password Rule-1

--- UnitTesting-UserRegistration/User.cs
+++ UnitTesting-UserRegistration/User.cs
@@ -7,6 +7,7 @@ namespace UnitTesting_UserRegistration
         public static string USER_NAME = "^[A-Z]{1}[a-z]{2,}";
         public static string EMAIL = "^[A-Za-z]{3,}[.]{0,}[a-zA-Z]{3,}[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}[.]{1}[a-z]{2,}$";
         public static string MOBILE = "^[0-9]{2}\\s[6-9]{1}[0-9]{9}$";
+        public static string PASSWORD = "^[A-Za-z0-9]{8,}";
         public string UserFirst(string name)
         {
             if (Regex.IsMatch(name, USER_NAME))
@@ -28,6 +29,12 @@ namespace UnitTesting_UserRegistration
         public string Mobile(string mobile)
         {
             if (Regex.IsMatch(mobile, MOBILE))
+                return "Valid";
+            return "InValid";
+        }
+        public string Password(string password)
+        {
+            if (Regex.IsMatch(password, PASSWORD))
                 return "Valid";
             return "InValid";
         }
